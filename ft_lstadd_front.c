e99425fed4f95a5463d3315ac98986efd292d1ac
Kevin Lopez <klopez@c2r3p1.42perpignan.fr>
2023-10-12 19:39:25 +0200

    libft

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,14 +6,14 @@
 /*   By: klopez <klopez@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 13:37:32 by klopez            #+#    #+#             */
-/*   Updated: 2023/10/11 13:40:50 by klopez           ###   ########.fr       */
+/*   Updated: 2023/10/12 19:25:10 by klopez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-    new->next = *lst;
-    *lst = new;
+	new->next = *lst;
+	*lst = new;
 }
